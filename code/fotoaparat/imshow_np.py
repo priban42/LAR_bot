aa42aacd8e357bdda62ca79af281ec7817d28aa3
Vojta <vojtech.priban@gmail.com>
2023-03-01 20:52:46 +0100

    im show + grayscale

--- code/fotoaparat/imshow_np.py
+++ code/fotoaparat/imshow_np.py
@@ -1,22 +1,31 @@
 import numpy as np
 import cv2
 import time
-name = "23_60_17_58_19.npy"
+import os
+
+def display_color_image(path, name):
+    img = np.load(path + name, allow_pickle=True)
+    img = img/255
+    cv2.imshow(name, img)
+    cv2.waitKey()
+
+def display_depth_image(path, name):
+    img = np.load(path + name, allow_pickle=True)
+    grayscale = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    grayscale[:, :, 0] = img*5
+    grayscale[:, :, 1] = img*5
+    grayscale[:, :, 2] = img*5
+    #print(grayscale)
+    cv2.imshow(name, grayscale)
+    cv2.waitKey()
+
 path = "/home.nfs/pribavoj/PycharmProjects/LAR_bot/code/fotoaparat/color_pictures/"
+path = "color_pictures/"
+path = "depth_pictures/"
+#name = "23_60_17_58_19.npy"
 
-img = np.load(path + name, allow_pickle=True)
-#print(img)
-#print(img.shape)
-img = img/255
-cv2.imshow("image", img)
-cv2.waitKey()
-imga = np.zeros([5,5,3])
+#print(os.listdir(path))
+for name in os.listdir(path):
+    #display_color_image(path, name)
+    display_depth_image(path, name)
 
-imga[:,:,0] = np.ones([5,5])*64/255.0
-imga[:,:,1] = np.ones([5,5])*128/255.0
-imga[:,:,2] = np.ones([5,5])*192/255.0
-print(imga.shape)
-print(imga)
-cv2.imwrite('color_img.jpg', imga)
-cv2.imshow("image", imga)
-cv2.waitKey()
